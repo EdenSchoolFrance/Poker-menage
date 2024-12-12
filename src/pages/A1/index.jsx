e35e6c8f1aa2dda0ce6fca238673698c277f38e6
tdimnet <thomas.dimnet@gmail.com>
2024-12-12 19:48:00 +0100

    Adding startSortingFunction

--- src/pages/A1/index.jsx
+++ src/pages/A1/index.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react"
+import { NavLink } from "react-router";
+import { useState } from "react";
 
 import Main from "../../components/Main";
 import H1 from "../../components/H1";
@@ -7,39 +8,56 @@ import Student from "../../components/Student";
 import { STUDENTS } from "./data";
 
 function Page() {
-  const [selectedStudents, setSelectedStudents] = useState(Array.from(STUDENTS))
+  const [selectedStudents, setSelectedStudents] = useState(
+    Array.from(STUDENTS),
+  );
 
   function isStudentAvailable(student) {
-    return selectedStudents.some(s => s === student)
+    return selectedStudents.some((s) => s === student);
   }
 
   function onSelectedStudent(selectedStudent) {
-    let newSelectedStudents = []
+    let newSelectedStudents = [];
 
     if (isStudentAvailable(selectedStudent)) {
-      newSelectedStudents = selectedStudents.filter(student => student !== selectedStudent)
+      newSelectedStudents = selectedStudents.filter(
+        (student) => student !== selectedStudent,
+      );
     } else {
-      newSelectedStudents = [...selectedStudents, selectedStudent]
-      console.log("Bar")
+      newSelectedStudents = [...selectedStudents, selectedStudent];
     }
 
-    setSelectedStudents(newSelectedStudents)
+    setSelectedStudents(newSelectedStudents);
+  }
+
+  function onChooseStudents() {
+    console.log("===")
+    console.log("Ok, let's go!")
+    console.log("===")
+    
+    console.log(selectedStudents)
   }
 
   return (
     <Main>
       <div className="w-6/12">
+        <NavLink className="text-white text-lg mb-8 block" to="/">
+          Revenir à l'accueil
+        </NavLink>
         <H1 css="mb-20" content="Bienvenue chez les A1" />
-        <ul className="grid grid-cols-4 gap-4">
+        <ul className="grid grid-cols-4 gap-4 mb-12">
           {STUDENTS.map((student) => (
             <Student
               key={student}
               name={student}
-              onClick={() => onSelectedStudent(student)}$
+              onClick={onSelectedStudent}
               isAvailable={isStudentAvailable(student)}
-            / >
+            />
           ))}
         </ul>
+        <button onClick={onChooseStudents} className="border-2 border-white mx-auto block py-2 px-4 rounded-lg text-white font-bold">
+          Lancer le tirage au sort
+        </button>
       </div>
     </Main>
   );
