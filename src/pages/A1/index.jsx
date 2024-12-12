d389a5a3fc7b0fb7b00678811c7571acf306fca7
tdimnet <thomas.dimnet@gmail.com>
2024-12-12 16:02:28 +0100

    Setting up React Router and creating needed routes

---
+++ src/pages/A1/index.jsx
@@ -0,0 +1,9 @@
+function Page() {
+    return (
+        <main>
+            <h1>Hello, A1!</h1>
+        </main>
+    )
+}
+
+export default Page
---
+++ src/pages/A2/index.jsx
@@ -0,0 +1,9 @@
+function Page() {
+    return (
+        <main>
+            <h1>Hello, A2!</h1>
+        </main>
+    )
+}
+
+export default Page
